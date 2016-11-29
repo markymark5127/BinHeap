1c72600442da16ff8104818aa4f3c5bc428decb7
markymark5127 <markmayne@ymail.com>
2016-11-29 14:39:04 -0500

    set up functions

--- BinHeap.cpp
+++ BinHeap.cpp
@@ -8,93 +8,155 @@
 /* **************************************************************** */
 
 #if CONSTRUCTOR || ALL
-//template <class T>
-//BinHeap<T>::BinHeap()
-//{
+template <class T>
+BinHeap<T>::BinHeap()
+{
     // TODO: Allocate initial heap array and store sentinel value
-//}
+}
 #endif
 
 /* **************************************************************** */
 
 #if DESTRUCTOR || ALL
-//template <class T>
-//BinHeap<T>::~BinHeap()
-//{
+template <class T>
+BinHeap<T>::~BinHeap()
+{
     // TODO: Deallocate heap array
-//}
+}
 #endif
 
 /* **************************************************************** */
 
 #if ISEMPTY || ALL
 // TODO: isEmpty() method
+template <class T>
+bool BinHeap<T>::isEmpty()
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
 
 #if MAKEEMPTY || ALL
 // TODO: makeEmpty() method
+template <class T>
+void BinHeap<T>::makeEmpty()
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
 
 #if RESIZEARRAY || ALL
 // TODO: resizeArray() method
+template <class T>
+void BinHeap<T>::resizeArray(int newSize)
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
 
 #if LEFTINDEX || ALL
 // TODO: leftIndex() method
+template <class T>
+int BinHeap<T>::leftIndex(int idx)
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
+
 #endif
 
 /* **************************************************************** */
 
 #if RIGHTINDEX || ALL
 // TODO: rightIndex() method
+template <class T>
+int BinHeap<T>::rightIndex(int idx)
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
+
 #endif
 
 /* **************************************************************** */
 
 #if PARENTINDEX || ALL
 // TODO: parentIndex() method
+template <class T>
+int BinHeap<T>::parentIndex(int idx)
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
 
 #if MINCHILD || ALL
 // TODO: minChild() method
+template <class T>
+int BinHeap<T>::minChild(int idx)
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
 
 #if INSERT || ALL
 // TODO: insert() method
+template <class T>
+void BinHeap<T>::insert(const T & x)
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
 
 #if REMOVEMIN || ALL
 // TODO: removeMin() method
+template <class T>
+T BinHeap<T>::removeMin()
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
 
 #if PERCOLATEUP || ALL
 // TODO: percolateUp() method
+template <class T>
+void BinHeap<T>::percolateUp(int idx)
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
 
 #if PERCOLATEDOWN || ALL
 // TODO: percolateDown() method
+template <class T>
+void BinHeap<T>::percolateDown(int idx)
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
 
 #if BUILDHEAP || ALL
 // TODO: buildHeap() method
+template <class T>
+void BinHeap<T>::buildHeap(const T* arr, int size)
+{
+    // TODO: Allocate initial heap array and store sentinel value
+}
 #endif
 
 /* **************************************************************** */
